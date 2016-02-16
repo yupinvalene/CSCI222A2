3080acafa55f8a6d176834768fc01956f1901dcf
Soon Yu Pin <yupinvalene@live.com.sg>
2016-02-17 00:07:02 +0800

    Added semicolon
    Added semicolon

--- Assn2/src/ClubFacilitiesBookingSystem.cpp
+++ Assn2/src/ClubFacilitiesBookingSystem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "hello everyone! jiayouuuu yo!"
+	cout << "hello everyone! jiayouuuu yo!";
 	return 0;
 
 }
