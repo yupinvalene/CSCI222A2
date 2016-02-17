ce1dff5a9625894ec3654dbbf59251a0bcb47ec2
yupinvalene <yupinvalene@live.com.sg>
2016-02-17 23:41:01 +0800

    Notification mutator function mutator implementation
    Notification mutator function minor changes as i forget to implement
    the mutator function

--- Assn2/src/Notification.cpp
+++ Assn2/src/Notification.cpp
@@ -22,6 +22,11 @@ Notification::Notification(string message, string notificationID,
 		string year, string time) {
 	this -> message = message;
 	this -> notificationID = notificationID;
+	this -> memberID = memberID;
+	this -> day = day;
+	this -> month = month;
+	this -> year = year;
+	this -> time = time;
 
 }
 
@@ -29,46 +34,67 @@ Notification::~Notification() {
 }
 
 string Notification::getMessage() {
+	return message;
 }
 
 string Notification::getNotificationID() {
+	return notificationID;
 }
 
 string Notification::getMemberID() {
+	return memberID;
 }
 
 string Notification::getDay() {
+	return day;
 }
 
 string Notification::getMonth() {
+	return month;
 }
 
 string Notification::getYear() {
+	return year;
 }
 
 string Notification::getTime() {
+	return time;
 }
 
-void Notification::setMessage(string allocator) {
+void Notification::setMessage(string message) {
+	this -> message = message;
 }
 
-void Notification::setNotificationID(string allocator) {
+void Notification::setNotificationID(string notificationID) {
+	this -> notificationID = notificationID;
 }
 
-void Notification::setMemberID(string allocator) {
+void Notification::setMemberID(string memberID) {
+	this -> memberID = memberID;
 }
 
-void Notification::setDay(string allocator) {
+void Notification::setDay(string day) {
+	this -> day = day;
 }
 
-void Notification::setMonth(string allocator) {
+void Notification::setMonth(string month) {
+	this -> month = month;
 }
 
-void Notification::setYear(string allocator) {
+void Notification::setYear(string year) {
+	this -> year = year;
 }
 
-void Notification::setTime(string allocator) {
+void Notification::setTime(string time) {
+	this -> time = time;
 }
 
 void Notification::toString() {
+	cout << "Message: " << message << endl
+		 << "NotificationID: " << notificationID << endl
+		 << "MemberID: " << memberID << endl
+		 << "Day: " << day << endl
+		 << "Month: " << month << endl
+		 << "Year: " << year << endl
+		 << "Time: " << time << endl << endl;
 }
