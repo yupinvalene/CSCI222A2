74881bec118f2b5e65ca4f9531d1b8244b722a3f
yupinvalene <yupinvalene@live.com.sg>
2016-02-20 09:10:54 +0800

    Revert "Revert "Initial design of member""
    This reverts commit 6ddb7fd450a7de07a475c47548850eace27fbf58.

---
+++ Assn2/src/Member.cpp
@@ -0,0 +1,119 @@
+
+/*
+ * Member.cpp
+ *
+ *  Created on: 17 Feb 2016
+ *      Author: yupin
+ */
+
+#include "Member.h"
+
+Member::Member() {
+	name = "";
+	address = "";
+	memberID = "";
+	password = "";
+	creditCardNo = "";
+
+}
+
+Member::Member(string name, string address, string memberID,
+		string password, string email, string contact,
+		string creditCardNo, Membership membership, vector<Notification> vectorOfNotification) {
+	this -> name = name;
+	this -> address = address;
+	this -> password = password;
+	this -> memberID = memberID;
+	this -> email = email;
+	this -> contact = contact;
+	this -> creditCardNo = creditCardNo;
+	this -> membership = membership;
+	this -> vectorOfNotification = vectorOfNotification;
+}
+
+
+Member::~Member() {
+
+}
+
+string Member::getName() {
+	return name;
+}
+
+string Member::getAddress() {
+	return address;
+}
+
+string Member::getMemberID() {
+	return memberID;
+}
+
+string Member::getPassword() {
+	return password;
+}
+
+string Member::getEmail() {
+	return email;
+}
+
+string Member::getContact() {
+	return contact;
+}
+
+string Member::getDOB() {
+	return dob;
+}
+
+string Member::getCreditCardNo() {
+	return creditCardNo;
+}
+
+void Member::setName(string name) {
+	this -> name = name;
+}
+
+void Member::setAddress(string address) {
+	this -> address = address;
+}
+
+void Member::setMemberID(string memberID) {
+	this -> memberID = memberID;
+}
+
+void Member::setPassword(string password) {
+	this -> password = password;
+}
+
+void Member::setEmail(string email) {
+	this -> email = email;
+}
+
+void Member::setContact(string contact) {
+	this -> contact = contact;
+}
+
+void Member::setDOB(string dob) {
+	this -> dob = dob;
+}
+
+void Member::setCreditCardNo(string creditCardNo)
+{
+	this -> creditCardNo = creditCardNo;
+}
+
+Membership Member::getMembership() {
+	return membership;
+}
+
+vector<Notification> Member::getNotification() {
+	return vectorOfNotification;
+}
+
+void Member::setNotification(vector<Notification> vectorOfNotification) {
+	this -> vectorOfNotification = vectorOfNotification;
+}
+
+void Member::setMembership(Membership membership) {
+	this -> membership = membership;
+
+}
