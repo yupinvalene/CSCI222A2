364cbcabffb432d15797378350ce6d9360f28cde
Louislohwk <louisloh1991@gmail.com>
2016-02-21 04:18:02 +0800

    Facility Handler
    Remove unnecessary stuff

--- Assn2/src/FacilityHandler.cpp
+++ Assn2/src/FacilityHandler.cpp
@@ -1,12 +1,5 @@
 #include "FacilityHandler.h"
 
-int main()
-{
-	cout << "test" << endl;
-	FacilityHandler fh;
-	fh.readData();
-}
-
 vector<Facility> FacilityHandler::getVectorOfFacilities()
 {
 	return vectorOfFacilities;
@@ -19,82 +12,139 @@ void FacilityHandler::setVectorOfFacilities(vector<Facility> vectorOfFacilities)
 
 void FacilityHandler::readData()
 {
+	FacilityHandler fh;
 	string data;
 	ifstream datafile;
 	datafile.open("facilitiesDatabase.txt");
 	getline(datafile, data, '\0'); // read the whole text file as string
 
-	string delimit = ",";
 	string delimiter = "\n";
+	char delim = ',';
 	size_t pos = 0;
-	size_t pos2 = -2;
-
+	size_t pos2 = 0;
 	string line;
-	string info;
-	long time;
-	int avail;
 
 	string id, name, add;
 	long sTime, eTime;
+	int avail;
+	vector<string> linesplit;
 
 	while ((pos = data.find(delimiter)) != string::npos) // split the data string by \n delimiter
 	{
 	    line = data.substr(0, pos);
-	    int i = 0;
-	    while ((pos2 = line.find(delimit)) != string::npos) // split the data string by , delimiter
-		{
-	    		cout << info << endl;
-				info = line.substr(0, pos2);
-				if(i == 0)
-				{
-					id = info;
-				}
-				if(i == 1)
-				{
-					name = info;
-				}
-				if(i == 2)
-				{
-					add = info;
-				}
-				if(i == 3)
-				{
-					sTime = atol(info.c_str());
-				}
-				if(i == 4)
-				{
-					eTime = atol(info.c_str());
-				}
-				line.erase(0, pos2 + delimit.length());
-				i++;
-		}
+	    fh.split(line, delim, linesplit);
 	    data.erase(0, pos + delimiter.length());
 	}
+	int i = 0;
+    for(string s : linesplit)
+    {
+    	if(i == 6)
+    	{
+    		i = 0;
+    	}
+       switch(i)
+       {
+           case 0: id = s; break;
+           case 1: name = s; break;
+           case 2: add = s; break;
+           case 3: sTime = atol(s.c_str()); break;
+           case 4: eTime = atol(s.c_str()); break;
+           case 5: avail = atoi(s.c_str()); break;
+       }
+       if(i == 5)
+       {
+    	   fh.addFacility(id, name, add, sTime, eTime, avail);
+       }
+       i++;
+    }
 
+}
+
+vector<string> FacilityHandler::split(const string &s, char delim, vector<string> &linesplit)
+{
+	stringstream ss(s);
+	string item;
+	while(getline(ss, item, delim))
+	{
+		linesplit.push_back(item);
+	}
+	return linesplit;
 }
 
 void FacilityHandler::writeData()
 {
+	ostringstream = oss;
 
+	for(Facility f : vectorOfFacilities)
+	{
+		string id = f.getFacID();
+		string name = f.getFacName();
+		string add = f.getFacAddress();
+		long sTime = f.getFacOperationStartTime();
+		long eTime = f.getFacOperationEndTime();
+		int avail = f.getAvailable();
+
+		oss << id << "," << name << "," << add << "," << sTime << "," << eTime << "," << avail << endl;
+	}
+
+	string info = oss.str();
+
+	ofstream outfile;
+	outfile.open("facilitiesDatabase.txt", ios::out | ios::trunc);
+	outfile << info;
+	outfile.close();
 }
 
-void FacilityHandler::addFacility(string facID, string facName, string facAddress, long facOperationStartTime,
-				long facOperationEndTime, bool available)
+void FacilityHandler::addFacility(string facID, string facName, string facAddress, long facOperationStartTime, long facOperationEndTime, bool available)
 {
-
+    Facility fac(facID, facName, facAddress, facOperationStartTime, facOperationEndTime, available);
+    vectorOfFacilities.push_back(fac);
 }
 
 void FacilityHandler::deleteFacility(string facID)
 {
-
+    int i = 0;
+    for(Facility f : vectorOfFacilities)
+    {
+        string id = f.getFacID();
+        if(id == facID)
+        {
+            vectorOfFacilities.erase(vectorOfFacilities.begin()+i);
+        }
+        i++;
+    }
 }
 
 Facility FacilityHandler::findFacility(string facID)
 {
-
+    int i = 0;
+    for(Facility f : vectorOfFacilities)
+    {
+        string id = f.getFacID();
+        if(id == facID)
+        {
+            return f;
+        }
+        i++;
+    }
 }
 
 string FacilityHandler::getAvailableFacilities()
 {
+    ostringstream os;
+	 for(Facility f : vectorOfFacilities)
+	 {
+	     string id = f.getFacID();
+	     int a = f.getAvailable();
+	     os << "Facilities Available\n===========================\n";
 
+	     if(a == 1)
+	     {
+	     	ostringstream os;
+	     	os << id << endl;
+	     }
+	 }
+	 string info;
+	 info = os.str();
+	 return info;
 }
