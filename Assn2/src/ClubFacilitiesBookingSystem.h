f5e185cbaf3be0ef213b341e5666ab5104ea91a4
yupinvalene <yupinvalene@live.com.sg>
2016-02-22 10:44:27 +0800

    Implementation of clubfacilitiesbookingsystem.h

---
+++ Assn2/src/ClubFacilitiesBookingSystem.h
@@ -0,0 +1,40 @@
+/*
+ * ClubFacilitiesBookingSystem.h
+ *
+ *  Created on: 22 Feb 2016
+ *      Author: yupin
+ */
+
+#ifndef CLUBFACILITIESBOOKINGSYSTEM_H_
+#define CLUBFACILITIESBOOKINGSYSTEM_H_
+
+#include "ServicesHandler.h"
+#include "FacilityHandler.h"
+#include "MembershipBuilder.h"
+#include "MemberHandler.h"
+
+using namespace std;
+
+class ClubFacilitiesBookingSystem{
+	public:
+		ClubFacilitiesBookingSystem();
+		~ClubFacilitiesBookingSystem();
+		void run();
+		void displayMemberMenu();
+		void displayManagerMenu();
+		void createClubMember();
+		void createClubFacility();
+		void createClubMembership();
+		void createClubService();
+		vector<string> split(const string &s, char delim, vector<string> &linesplit);
+
+	private:
+
+		ServicesHandler serviceHandler;
+		FacilityHandler facilityHandler;
+		MembershipBuilder membershipBuilder;
+		MemberHandler memberHandler;
+
+};
+
+#endif /* CLUBFACILITIESBOOKINGSYSTEM_H_ */
